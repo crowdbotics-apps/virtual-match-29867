f0313c0c229bb402e00412adadb9d3f44eafa3eb
Crowdbotics <team@crowdbotics.com>
2021-08-17 01:46:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "virtual_match_29867Identifier",
+  url: "https://virtual_match_29867Identifier.botics.co",
+  api: "https://virtual_match_29867Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
